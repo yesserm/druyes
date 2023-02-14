64da1c6b16ff16f6bdfe535fb1e98e9b613877f9
Yesser Miranda <yesser.miranda@uraccan.edu.ni>
2023-02-14 11:48:26 -0600

    add sass support to theme - setup gulp in gulpfile.js - adding bootstrap support

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,30 @@ let gulp = require('gulp');
 let browserSync = require('browser-sync').create();
 let reload = browserSync.reload;
 let connect = require('gulp-connect-php');
-let sass = require('gulp-sass');
+let sass = require('gulp-sass')(require('node-sass'));
+let sourcemaps = require('gulp-sourcemaps');
+let autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('watch', function (){
+// definir la tarea de gulp
+gulp.task('sass', done => {
+  gulp.src('./scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+
+  done();
+});
+
+//gulp.task('watch', function (){
+gulp.task('watch', done => {
+  gulp.watch('./scss/**/*.scss').on('change', gulp.series('sass'));
+  gulp.watch('./css/**/*.css').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
   gulp.watch('./templates/**/*.html.twig').on('change', browserSync.reload);
   connect.server({}, function (){
     browserSync.init({
-      proxy: 'https://d9.ddev.site'
+      proxy: 'https://d9.ddev.site',
     })
   });
+  done();
 });
--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 console.log("HOLA THEME DRUYES");
+
+(function ($, Drupal){
+  Drupal.behaviors.druyes = {
+    attach: function attach(context){
+      $(".jqactivo").click(function (){
+        $(this).fadeOut();
+      });
+    }
+  };
+})(jQuery, Drupal);
